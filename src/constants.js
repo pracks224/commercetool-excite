dd68e0fe548625b1556d786fed221dda6c827e2b
Prakash Sharma <psharma@Alghanim.com>
2025-07-28 15:45:23 +0300

    mc script failing

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'xcite-custom-dashboard';
+export const entryPointUriPath = 'custom-xcite-dashboard';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
